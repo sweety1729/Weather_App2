f1962e3da942de2083e3270b2a619c03ee5bbd2e
sweety1729 <madhumitha44240@gmail.com>
2024-06-08 18:02:52 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,104 +1,109 @@
-package com.example.weather_app;
+package com.example.quiz_app2;
 
+import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.*;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity {
-    TextView cityName;
-    Button search;
-    TextView show;
-    String url;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView questionTextView;
+    TextView totalQuestionTextView;
+    Button ansA,ansB,ansC,ansD;
+    Button btn_submit;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex =0;
+    String selectedAnswer="";
 
 
-    class getWeather extends AsyncTask<String, Void, String>{
-        @Override
-        protected String doInBackground(String... urls){
-            StringBuilder result = new StringBuilder();
-            try{
-                URL url= new URL(urls[0]);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line="";
-                while((line = reader.readLine()) != null){
-                    result.append(line).append("\n");
-                }
-                return result.toString();
-            }catch(Exception e){
-                e.printStackTrace();
-                return null;
-            }
-        }
-        @Override
-        protected void onPostExecute(String result){
-            super.onPostExecute(result);
-            try{
-                JSONObject jsonObject = new JSONObject(result);
-                String weatherInfo = jsonObject.getString("main");
-                weatherInfo = weatherInfo.replace("temp", "Temperature");
-                weatherInfo = weatherInfo.replace("feels_like", "feels_like");
-                weatherInfo = weatherInfo.replace("temp_max", "Temperature Max");
-                weatherInfo = weatherInfo.replace("temp_min", "Temperature Min");
-                weatherInfo = weatherInfo.replace("pressure", "Pressure");
-                weatherInfo = weatherInfo.replace("humidity", "Humidity");
-                weatherInfo = weatherInfo.replace("{", "");
-                weatherInfo = weatherInfo.replace("}", "");
-                weatherInfo = weatherInfo.replace(",", "\n");
-                weatherInfo = weatherInfo.replace(":", " : ");
-                show.setText(weatherInfo);
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cityName = findViewById(R.id.citYName);
-        search = findViewById(R.id.search);
-        show = findViewById(R.id.weather);
 
-        final String[] temp={""};
+        totalQuestionTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_a);
+        ansB = findViewById(R.id.ans_b);
+        ansC = findViewById(R.id.ans_c);
+        ansD = findViewById(R.id.ans_d);
+        btn_submit = findViewById(R.id.btn_submit);
 
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
-                String city = cityName.getText().toString();
-                try{
-                    if(city!=null){
-                        url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=c30970023f311dbc760aec17ad4aca08";
-                    }else{
-                        Toast.makeText(MainActivity.this, "Enter City", Toast.LENGTH_SHORT).show();
-                    }
-                    getWeather task= new getWeather();
-                    temp[0] = task.execute(url).get();
-                }catch(ExecutionException e){
-                    e.printStackTrace();
-                }catch(InterruptedException e){
-                    e.printStackTrace();
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        btn_submit.setOnClickListener(this);
+
+        totalQuestionTextView.setText("Total question: "+totalQuestion);
+
+        loadNewQuestion();
+    }
+
+    private void loadNewQuestion(){
+        if(currentQuestionIndex == totalQuestion){
+            finishQuiz();
+            return;
+        }
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+
+        selectedAnswer="";
+
+    }
+
+    private void finishQuiz(){
+        String passStatus;
+        if(score >= totalQuestion*0.6){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Your Score is "+score+" Out of "+totalQuestion)
+                .setPositiveButton("Restart",((dialog, i) -> restartQuiz() ))
+                .setCancelable(false)
+                .show();
+    }
+
+    private void restartQuiz(){
+        score = 0;
+        currentQuestionIndex=0;
+        loadNewQuestion();
+    }
+
+    @Override
+    public void onClick(View view){
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+
+        if(clickedButton.getId() == R.id.btn_submit) {
+            if(!selectedAnswer.isEmpty()){
+                if(selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
+                    score++;
+                }else{
+                    clickedButton.setBackgroundColor(Color.MAGENTA);
                 }
-                if(temp[0] == null){
-                    show.setText("Cannot able to find Weather");
-                }
+                currentQuestionIndex++;
+                loadNewQuestion();
+            }else{
+
             }
-        });
-
+        }
+        else{
+            selectedAnswer=clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
